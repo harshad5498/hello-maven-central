43e6366b8f490183ebb23644fe0844539f09bbd0
harshad5498 <60310681+harshad5498@users.noreply.github.com>
2020-09-04 17:41:25 +0530

    initial commit

---
+++ src/main/java/com/github/harshad5498/hellomavencentral/component/UserComponent.java
@@ -0,0 +1,30 @@
+package com.github.harshad5498.hellomavencentral.component;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserComponent {
+	
+	private HashMap<Integer,String> users = new HashMap<Integer,String>();
+	UserComponent(){
+		users.put(1, "Tony Stark (Iron Man)");
+		users.put(2, "Barry Allen (The Flash)");
+		users.put(3, "Eobard Thawne (Revese Flash)");
+		users.put(4, "Steve Rogers (Captain America)");
+		users.put(5, "Bruce Banner (The Hulk)");
+		users.put(6, "Oliver Queen (Green Arrow)");
+		users.put(7, "Clark Kent (SuperMan)");
+		users.put(8, "Peter Parker (SpiderMan)");
+		
+	}
+	
+	public HashMap<Integer,String> getUsers() {
+		return users;
+	}
+	
+	public String getUserNameById(Integer id) {
+		return users.get(id);
+	}
+}
---
+++ src/test/java/com/paramatrix/hellomavencentral/HelloMavenCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramatrix.hellomavencentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMavenCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
